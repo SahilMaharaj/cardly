bc864bdc5a7ac25f01573286078259ec3aa5226b
SahilMaharaj <maharajsahil9@gmail.com>
2020-05-07 21:25:39 +0200

    Trying multiple rel attributes

--- src/main.js
+++ src/main.js
@@ -8,8 +8,7 @@ export default function (Vue, { router, head, isClient }) {
   })
   Vue.component('Layout', DefaultLayout)
   head.link.push ({
-    rel: 'preload',
-    as: 'style',
+    rel: 'stylesheet preload',
     href: 'https://fonts.googleapis.com/css2?family=DM+Sans:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&display=swap'
   })
 }
